35e359dd5fa256609fcb397b01f1a441563f421a
Ian Peebles <ip117417@ohio.edu>
2022-01-14 13:41:20 -0500

    Initial code

--- filesize.cc
+++ filesize.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 using namespace std;
 
 // output format to use IDENTICALLY:
@@ -8,11 +9,23 @@ using namespace std;
 // input.3lines: 3
 // input.1line: 1
 
-int main( int argc, char* argv[] )
-{
+int main( int argc, char* argv[] ){
+    cout << "program: " << argv[0] << endl;
+    int numlines = 0;
+    string buffer;
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
-
+    for (int arg = 1; arg < argc; ++arg){
+            //std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+        numlines = 0;
+        string filename = argv[arg];
+        ifstream ins;
+        ins.open(filename);
+        while (!ins.eof()){
+            numlines++;
+            getline(ins, buffer);
+        }
+        ins.close();
+        cout << filename << ": " << numlines << endl;
+    }
     exit(0); // this means that the program executed correctly!
 }
